221920dfc30d22638237dda55837973447aa4322
Fitra Aditya <fitra.pradana@mekari.com>
2023-09-07 12:45:09 +0700

    Fix option initialization.

--- lib/pigeon/core.rb
+++ lib/pigeon/core.rb
@@ -10,6 +10,7 @@ begin
   require 'mime/types'
   require 'net/http'
   require 'openssl'
+  require 'retryable'
   require 'securerandom'
   require 'stringio'
   require 'uri'
@@ -32,7 +33,8 @@ module Pigeon
     time_window:          10,
     sleep_window:         10,
     retryable:            true,
-    retry_threshold:      3
+    retry_threshold:      3,
+    monitoring:           false
   }
   DEFAULT_CALLBACKS = {
     CircuitBreakerOpen:  nil,
@@ -50,85 +52,76 @@ module Pigeon
       @options   = DEFAULT_OPTIONS
       @callbacks = DEFAULT_CALLBACKS
 
-      @options['request_name'] = name
+      @options[:request_name] = name
 
       opts.each do |k, v|
         raise Error::Argument, "unknown option #{k}" unless VALID_OPTIONS.include?(k.to_s)
-        @options[k] = v
+        @options[k.to_sym] = v
       end
 
       clbk.each do |k, v|
         raise Error::Argument, "unknown callback #{k}" unless VALID_CALLBACKS.include?(k.to_s)
-        @callbacks[k] = v
+        @callbacks[k.to_sym] = v
       end
 
-      if @options['retryable']
+      if @options[:retryable]
         Retryable.configure do |config|
           config.sleep = lambda { |n| 4**n }
         end
-      end
-
-      Datadog.configure do |c|
-        c.env     = ENV['DD_ENV']
-        c.service = ENV['DD_SERVICE']
-        c.tracing.partial_flush.enabled = true
-        c.profiling.enabled = false
-        c.runtime_metrics.enabled = false
-        c.runtime_metrics.statsd = Datadog::Statsd.new
-        c.tracing.sampler = Datadog::Tracing::Sampling::PrioritySampler.new(
-                              post_sampler: Datadog::Tracing::Sampling::RuleSampler.new(
-                                              [Datadog::Tracing::Sampling::SimpleRule.new(service: ENV['DD_SERVICE'], sample_rate: 1.0)]
-                              )
-        )
       end
     end
 
     def get url, args = {}
       start = Time.now
       response = http(:get, url, args)
-      Pigeon::Statsd.new(@options['request_name'] + '_latency', tags: [url]).capture(action: :histogram, count: (Time.now - start))
+      Pigeon::Statsd.new(@options[:request_name] + '_latency', tags: [url]).capture(action: :histogram, count: (Time.now - start)) unless @options[:monitoring]
 
       response
     rescue => e
-      @callbacks['HttpError']&.call(e)
+      @callbacks[:HttpError]&.call(e)
     end
 
     def post url, args = {}
       start = Time.now
       response = http(:post, url, args)
-      Pigeon::Statsd.new(@options['request_name'] + '_latency', tags: [url]).capture(action: :histogram, count: (Time.now - start))
+      puts @options
+      Pigeon::Statsd.new(@options[:request_name] + '_latency', tags: [url]).capture(action: :histogram, count: (Time.now - start)) unless @options[:monitoring]
 
       response
     rescue => e
-      @callbacks['HttpError']&.call(e)
+      @callbacks[:HttpError]&.call(e)
     end
 
     def put url, args = {}
-      http(:put, url, args)
+      start = Time.now
+      response = http(:put, url, args)
+      Pigeon::Statsd.new(@options[:request_name] + '_latency', tags: [url]).capture(action: :histogram, count: (Time.now - start)) unless @options[:monitoring]
+
+      response
     rescue => e
-      @callbacks['HttpError']&.call(e)
+      @callbacks[:HttpError]&.call(e)
     end
 
     def delete url, args = {}
       http(:delete, url, args)
     rescue => e
-      @callbacks['HttpError']&.call(e)
+      @callbacks[:HttpError]&.call(e)
     end
 
     def http method, url, args = {}
       args.merge({
-        read_timeout: @options['request_timeout'],
-        open_timeout: @options['request_open_timeout'],
-        ssl_verify:   @options['ssl_verify']
+        read_timeout: @options[:request_timeout],
+        open_timeout: @options[:request_open_timeout],
+        ssl_verify:   @options[:ssl_verify]
       })
       response = Pigeon::Http::Request.new(method, url, args).execute
-      Pigeon::Statsd.new(@options['request_name'] + '_througput', tags: [url]).capture
+      Pigeon::Statsd.new(@options[:request_name] + '_througput', tags: [url]).capture
 
       response
     end
 
     def circuit
-      Circuitbox.circuit(@options['request_name'], exceptions: [HTTP::ConnectionError, HTTP::HeaderError, HTTP::RequestError, HTTP::ResponseError, HTTP::TimeoutError])
+      Circuitbox.circuit(@options[:request_name], exceptions: [HTTP::ConnectionError, HTTP::HeaderError, HTTP::RequestError, HTTP::ResponseError, HTTP::TimeoutError])
     end
   end
 
