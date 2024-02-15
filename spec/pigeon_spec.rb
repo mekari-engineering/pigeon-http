267549defbabdbc140bb551367cabd4fa6007f5a
Fitra Aditya <fitra.pradana@mekari.com>
2024-02-15 09:23:47 +0700

    Fix rubocop.

--- spec/pigeon_spec.rb
+++ spec/pigeon_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe Pigeon do
-  it "has a version number" do
+  it 'has a version number' do
     expect(Pigeon::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it 'does something useful' do
     expect(false).to eq(true)
   end
 end
